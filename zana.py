effa4ab7e60cccba1b050fdf3c6400a4164f2408
Apoeip43 <henrique.aroeira43@gmail.com>
2025-11-23 06:07:14 -0300

    made it so other users can see the resulting message now

--- zana.py
+++ zana.py
@@ -177,7 +177,7 @@ async def wiki(interaction: dc.Interaction, item: str):
         return
     # await scrape_wiki_for_item_card(url)
     # sends the response once ready
-    await interaction.followup.send(embed=embed)
+    await interaction.response.send_message(embed=embed)
 
 @wiki.autocomplete("item")
 async def wiki_autocomplete(interaction: dc.Interaction, current: str):
