b1f931becd30c692e6f956da1b6581158fea2e19
Apoeip43 <henrique.aroeira43@gmail.com>
2025-12-08 03:34:51 -0300

    forgot to turn match object into list of matches

--- zana.py
+++ zana.py
@@ -81,21 +81,25 @@ async def check_message_for_embedded_wiki_query(ctx: dc.message.Message):
     inside_link = wikilink
     pattern_poe1 = r"\[\[([^<[]*?)\]\]"
     matches_poe1 = re.search(pattern_poe1, ctx.content)
+    matches_poe1 = matches_poe1 if matches_poe1 else []
     pattern_poe2 = r"<<([^<[]*?)>>"
     matches_poe2 = re.search(pattern_poe2, ctx.content)
+    matches_poe2 = matches_poe2 if matches_poe2 else []
     closest_match = ""
 
     matchlist1 = []
     matchlist2 = []
 
     if matches_poe1:
-        for m in matches_poe1.groups():
+        matches_poe1 = matches_poe1.groups()
+        for m in matches_poe1:
             ret = await search_wiki_titles(m, limit = 5)
             closest_match = ret[0] if ret else ""
             matchlist1.append((closest_match, wikilink, False)) if closest_match else None
         is_poe2 = False
     elif matches_poe2:
-        for m in matches_poe2.groups():
+        matches_poe2 = matches_poe2.groups()
+        for m in matches_poe2:
             ret = await search_wiki_titles(m, limit = 5, searchlink_internal=searchlink2)
             closest_match = ret[0] if ret else ""
             matchlist2.append((closest_match, wikilink2, True)) if closest_match else None
